5094783c88cdd44d7891e1d03f505bb99af0f191
AslanKussein <aslan_kusain93@mail.ru>
2018-03-28 14:06:33 +0600

    Update locale.js

--- src/main/webapp/js/locale.js
+++ src/main/webapp/js/locale.js
@@ -1,32 +1,28 @@
-function locale() {
-    webix.i18n.locales["ru-RU"] = {
-        groupDelimiter: " ",
-        groupSize: 3,
-        decimalDelimiter: ",",
-        decimalSize: 2,
-        dateFormat: "%d.%m.%Y",
-        timeFormat: "%H:%i",
-        longDateFormat: "%d %F %Y",
-        fullDateFormat: "%d.%m.%Y %H:%i",
-        price: "{obj} руб.",
-        priceSettings: null, //use number defaults
-
-        calendar: {
-            monthFull: ["Январь", "Февраль", "Март", "Апрель", "Maй", "Июнь", "Июль", "Август", "Сентябрь", "Oктябрь", "Ноябрь", "Декабрь"],
-            monthShort: ["Янв", "Фев", "Maр", "Aпр", "Maй", "Июн", "Июл", "Aвг", "Сен", "Окт", "Ноя", "Дек"],
-            dayFull: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
-            dayShort: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
-            hours: "Часы",
-            minutes: "Минуты",
-            done: "Гoтовo",
-            clear: "Очистить",
-            today: "Сегодня"
-        },
-        controls: {
-            select: "Выбрать"
-        }
+const keys = [];
+    document.onkeydown = e => {
+        if(keys.indexOf(e.which)<0) keys.push(e.which);
+    };
+    document.onkeyup = e => {
+        keys.splice(keys.indexOf(e.which),1);
     };
 
-    webix.i18n.setLocale('ru-RU');
-    webix.i18n.setLocale();
-}
+    setInterval(() => {
+        keys.forEach(item => {
+        switch(item) {
+        case 87:case 38://W
+            console.log('вперед');
+            break;
+        case 68:case 39://D
+            console.log('вправо');
+            break;
+        case 65:case 37://A
+            console.log('влево');
+            break;
+        case 83:case 40://S
+            console.log('назад');
+            break;
+        default:
+            //console.log(item);
+        }
+    });
+    }, 111);
