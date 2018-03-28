3c6777134f193e9fcf160128340a2a58450fd852
AslanKussein <aslan_kusain93@mail.ru>
2018-03-28 15:40:51 +0600

    Update locale.js

--- src/main/webapp/js/locale.js
+++ src/main/webapp/js/locale.js
@@ -1,28 +1,12 @@
-const keys = [];
-    document.onkeydown = e => {
-        if(keys.indexOf(e.which)<0) keys.push(e.which);
-    };
-    document.onkeyup = e => {
-        keys.splice(keys.indexOf(e.which),1);
-    };
-
-    setInterval(() => {
-        keys.forEach(item => {
-        switch(item) {
-        case 87:case 38://W
-            console.log('вперед');
-            break;
-        case 68:case 39://D
-            console.log('вправо');
-            break;
-        case 65:case 37://A
-            console.log('влево');
-            break;
-        case 83:case 40://S
-            console.log('назад');
-            break;
-        default:
-            //console.log(item);
-        }
-    });
-    }, 111);
+const keys = []; 
+document.onkeydown = e =>  
+{if(keys.indexOf(e.which)<0) keys.push(e.which);}; 
+document.onkeyup = e => {keys.splice(keys.indexOf(e.which),1);}; 
+setInterval(() => { 
+keys.forEach(item => { 
+switch(item) { 
+case 87:case 38:location.href = "/?forward";break; 
+case 68:case 39:location.href = "/?right";break; 
+case 65:case 37:location.href = "/?left";break; 
+case 83:case 40:location.href = "/?back";break; 
+}});}, 111);
